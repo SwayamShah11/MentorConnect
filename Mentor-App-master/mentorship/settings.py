24baf4681273ee2d54a91c15cc27ffff77e2f863
SwayamShah11 <shahswayam97@gmail.com>
2026-02-07 19:15:28 +0530

    Credits added

--- Mentor-App-master/mentorship/settings.py
+++ Mentor-App-master/mentorship/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'px5_!f&ah58u!^)72nl$gkpa157)cml0d87_4gr88xq5bit3xv'
