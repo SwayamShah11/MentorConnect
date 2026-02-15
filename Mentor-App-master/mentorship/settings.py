6d0c715d3ab4ec6c5354b9b264cd0f16ad039e5f
SwayamShah11 <shahswayam97@gmail.com>
2026-02-15 22:26:12 +0530

    Minor updates

--- Mentor-App-master/mentorship/settings.py
+++ Mentor-App-master/mentorship/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'px5_!f&ah58u!^)72nl$gkpa157)cml0d87_4gr88xq5bit3xv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
